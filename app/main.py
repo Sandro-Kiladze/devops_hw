a7c5c39c3207fe9318aa24d9b92670962646075c
Sandro-Kiladze <sandrokiladze7@gmail.com>
2025-04-14 23:55:02 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/hello")
-def read_hello():
-    return {"message": "Hello World!"}
+from flask import Flask
+app = Flask(__name__)
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
